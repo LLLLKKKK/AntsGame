2316bd4afeab32619d25487e5f158ab74bcbc7d2
aliaw <aliaw@sfu.ca>
2013-03-19 18:54:07 -0700

    Create setting.py

---
+++ setting.py
@@ -0,0 +1,26 @@
+#VARIABLE
+# This is to make it easy to change variables in the future.
+
+# GENERAL
+SIZE =  width, height = 800, 600
+FPS = 12 # frames per second.
+TIME_SCALE = 1 # Time Scale
+
+# GAME
+MAX_ANT = 200
+ANT_SPAWN_RATE = 0.5
+FOOD_INCREASE_SPAWN_RATE = 0.1
+MAX_FOOD = 30
+FOOD_RANGE = 5
+MAX_PHEROMONE = 5
+VISION = 20
+FOOD_SPAWN_RATE = 1
+PHEROMONE_LIFETIME = 10
+
+# AI
+PHEROMONE_RANGE = 40
+PLAYER_SPREAD_RADIUS = 50 # Sepcify the area around the hive
+# that player ants will spread around.
+
+
+#VARIABLE
